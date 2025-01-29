e2281c8c228a456a925f7c4e57980dfa4b9d59c7
JackAuty <jack.auty@utas.edu.au>
2025-01-30 09:53:57 +1100

    Initial commit

--- Graphfunction.R
+++ Graphfunction.R
@@ -1,2 +1,15 @@
 library(devtools)
-create_package("C:/Users/jrivers/Dropbox/Code/R_package_JRA/Boi_Raincloud")
+#Create the empty package
+create_package("C:/Users/jrivers/Dropbox/Code/R_package_JRA/BoiRaincloud")
+
+#Configure my git by providing user name and email
+use_git_config(user.name = "JackAuty", user.email = "jack.auty@utas.edu.au")
+
+#Check if Git is working
+usethis::git_sitrep()
+
+#It didn't like that my token was in my .Renviron, so delete my git from there using this
+usethis::edit_r_environ()
+
+usethis::git_vaccinate()
+use_git()
