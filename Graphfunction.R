faecd3fc4ff779e24828ba1e1205ba4945cd9abd
JackAuty <jack.auty@utas.edu.au>
2025-01-30 10:00:56 +1100

    Initial commit

--- Graphfunction.R
+++ Graphfunction.R
@@ -10,6 +10,28 @@ usethis::git_sitrep()
 
 #It didn't like that my token was in my .Renviron, so delete my git from there using this
 usethis::edit_r_environ()
+#Restart R to take effected
+rstudioapi::restartSession()
 
+#Check if Git is working
+usethis::git_sitrep()
+
+#Need to vaccinate?
 usethis::git_vaccinate()
-use_git()
+
+#Need a token to access git
+#Create token
+usethis::create_github_token()
+
+#Save token
+gitcreds::gitcreds_set()
+
+#Check if Git is working
+usethis::git_sitrep()
+
+#Initialise the git
+usethis::use_git()
+usethis::use_github()
+
+#Check if Git is working
+usethis::git_sitrep()
