43959a9b982d117216b39c30604a55ba1f4f1de3
JackAuty <jack.auty@utas.edu.au>
2025-01-30 10:47:59 +1100

    Minor edits to the function to try get it to load

--- Graphfunction.R
+++ Graphfunction.R
@@ -1,13 +1,14 @@
-#' Raincould plot
+#' Raincloud plot
 #'
 #' @param x numerical vector
 #' @param y character or factor vector on how to split the data
 #'
 #' @returns Plot of x vs y
 #' @export
+#'
 #' @examples
-#' x<-c(1,2,3,4)
-#' y<-c("a","a","b","b")
-graph_boi<-function(x, y){
+#' x <- c(1, 2, 3, 4)
+#' y <- c("a", "a", "b", "b")
+graph_boi <- function(x, y) {
   plot(x ~ y)
-  }
+}
